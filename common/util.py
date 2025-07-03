ac665f83661805f5da3386571f7e607788e5229e
k20ku <heizhaokuan@gmail.com>
2025-07-03 14:25:48 +0900

    docs: add comment for preprocess

--- common/util.py
+++ common/util.py
@@ -2,6 +2,34 @@ import numpy as np
 
 
 def preprocess(text: str):
+    """preprocesser
+
+    preprocess text to corpus
+
+    Args:
+        コーパス化する改行なしの一文: str
+
+    Returns:
+        (単語IDのndarray, 単語から単語IDへのdict, 単語IDから単語へのdict)
+
+        For example:
+
+        >>> text = "You say goodbye and I say hello."
+
+        >>> corpus, word_to_id, id_to_word = preprocess(text)
+
+        >>> corpus
+        array([0, 1, 2, 3, 4, 1, 5, 6])
+
+        >>> word_to_id
+        {'you': 0, 'say': 1, 'goodbye': 2, 'and': 3,
+        'i': 4, 'hello': 5, '.': 6}
+        
+        >>> id_to_word
+        {0: 'you', 1: 'say', 2: 'goodbye', 3: 'and',
+        4: 'i', 5: 'hello', 6: '.'}
+
+    """
     words = text.lower().replace('.', ' .').split(' ')
 
     word_to_id: dict[str, int] = {}
